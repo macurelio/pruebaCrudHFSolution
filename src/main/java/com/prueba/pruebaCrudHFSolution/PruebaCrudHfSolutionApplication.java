e1bd16a71692d69e159c408d7bc987b7a3d78a8f
macurelio <marcoau01@gmail.com>
2025-09-17 15:16:32 -0300

    FIRST-COMMIT-CREATE-BRANCH

---
+++ src/main/java/com/prueba/pruebaCrudHFSolution/PruebaCrudHfSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.pruebaCrudHFSolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaCrudHfSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaCrudHfSolutionApplication.class, args);
+	}
+
+}
